4500036a5a8f3dca0915f6976b0695d5b937875d
amine alaoui <omari1amineco@gmail.com>
2024-01-11 18:43:23 +0100

    feat: add a useState to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import HeaderContainer from "@/components/HeaderContainer";
+import { useState } from "react";
 
 export default function Home() {
+  const [menuOpen, setMenuOpen] = useState();
   return (
     <div>
-      <HeaderContainer />
+      <HeaderContainer menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
     </div>
   );
 }
