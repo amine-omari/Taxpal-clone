8393d0b0df4665734e77c89f4ca39ebd1d8c7de0
amine alaoui <omari1amineco@gmail.com>
2024-01-11 16:13:54 +0100

    feat: add link item to HeaderContainer component

--- components/HeaderContainer.jsx
+++ components/HeaderContainer.jsx
@@ -1,12 +1,21 @@
 import LogoIcon from "@/icons/LogoIcon";
+import Link from "next/link";
 import React from "react";
 
 const HeaderContainer = () => {
   return (
     <header>
-      <nav className="px-4">
+      <nav className="flex items-center justify-between px-4">
         <div>
           <LogoIcon />
+        </div>
+        <div>
+          <Link
+            className="rounded-full bg-blue-600 px-4 py-2 text-sm font-semibold text-white"
+            href="/"
+          >
+            Get started
+          </Link>
         </div>
       </nav>
     </header>
