93e7d1a9b425141b5d56a2abe251d2e6b321634e
amine alaoui <omari1amineco@gmail.com>
2024-01-12 00:48:02 +0100

    feat: add the logo images to TrustedCompanies component

--- components/TrustedCompanies.jsx
+++ components/TrustedCompanies.jsx
@@ -1,10 +1,69 @@
+import Image from "next/image";
 import React from "react";
 
 const TrustedCompanies = () => {
+  const firstGroupLogo = [
+    {
+      id: 1,
+      src: "https://salient.tailwindui.com/_next/static/media/transistor.7274e6c3.svg",
+      alt: "",
+      width: 158,
+    },
+    {
+      id: 2,
+      src: "https://salient.tailwindui.com/_next/static/media/tuple.74eb0ae0.svg",
+      alt: "",
+      width: 105,
+    },
+    {
+      id: 3,
+      src: "https://salient.tailwindui.com/_next/static/media/statickit.d7937794.svg",
+      alt: "",
+      width: 127,
+    },
+  ];
+
+  const secondGroupLogo = [
+    {
+      id: 1,
+      src: "https://salient.tailwindui.com/_next/static/media/mirage.18d2ec4e.svg",
+      alt: "",
+      width: 138,
+    },
+    {
+      id: 2,
+      src: "https://salient.tailwindui.com/_next/static/media/laravel.7deed17e.svg",
+      alt: "",
+      width: 136,
+    },
+    {
+      id: 3,
+      src: "https://salient.tailwindui.com/_next/static/media/statamic.6da5ebfb.svg",
+      alt: "",
+      width: 147,
+    },
+  ];
+
   return (
     <div className="mt-36">
-      <div className="text-center">
+      <div className="text-center font-medium">
         <p>Trusted by these six companies so far</p>
+      </div>
+      <div className="mt-8 flex justify-center space-x-8">
+        <div className="space-y-8">
+          {firstGroupLogo.map(({ id, src, alt, width }) => (
+            <div className="flex items-center justify-center" key={id}>
+              <Image src={src} alt={alt} width={width} height={48} />
+            </div>
+          ))}
+        </div>
+        <div className="space-y-8">
+          {secondGroupLogo.map(({ id, src, alt, width }) => (
+            <div className="flex items-center justify-center" key={id}>
+              <Image src={src} alt={alt} width={width} height={48} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
