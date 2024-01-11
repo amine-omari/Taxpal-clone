1a86c0cf888af7542b0a296a068a14b26004bd7e
amine alaoui <omari1amineco@gmail.com>
2024-01-11 16:20:59 +0100

    feat: import MenuIcon to HeaderContainer comopnent

--- components/HeaderContainer.jsx
+++ components/HeaderContainer.jsx
@@ -1,4 +1,5 @@
 import LogoIcon from "@/icons/LogoIcon";
+import MenuIcon from "@/icons/MenuIcon";
 import Link from "next/link";
 import React from "react";
 
@@ -9,13 +10,16 @@ const HeaderContainer = () => {
         <div>
           <LogoIcon />
         </div>
-        <div>
+        <div className="flex items-center space-x-5">
           <Link
             className="rounded-full bg-blue-600 px-4 py-2 text-sm font-semibold text-white"
             href="/"
           >
             Get started
           </Link>
+          <button>
+            <MenuIcon />
+          </button>
         </div>
       </nav>
     </header>
