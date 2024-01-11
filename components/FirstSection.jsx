cb0c6bbe1023ac2fc5257b238a8cf0063d2f71a8
amine alaoui <omari1amineco@gmail.com>
2024-01-11 22:14:10 +0100

    feat: add style to the paragraph

--- components/FirstSection.jsx
+++ components/FirstSection.jsx
@@ -4,12 +4,16 @@ const FirstSection = () => {
   return (
     <section className="px-4 pb-16 pt-20">
       <div className="text-center">
-        <h1 className="leading-[55px] text-5xl font-semibold">
+        <h1 className="text-5xl font-semibold leading-[55px]">
           Accounting
           <br />
           <span className="text-blue-600">made simple</span> for small
           businesses.
         </h1>
+        <p className="mt-6 px-2 text-lg leading-8 text-slate-700">
+          Most bookkeeping software is accurate, but hard to use. We make the
+          opposite trade-off, and hope you don’t get audited.
+        </p>
       </div>
     </section>
   );
