c06e2b423e0ca82062249f384985e1e2a4bc1364
amine alaoui <omari1amineco@gmail.com>
2024-01-11 16:23:49 +0100

    fix: pading style in HeaderContainer component

--- components/HeaderContainer.jsx
+++ components/HeaderContainer.jsx
@@ -5,14 +5,14 @@ import React from "react";
 
 const HeaderContainer = () => {
   return (
-    <header>
+    <header className="py-10">
       <nav className="flex items-center justify-between px-4">
         <div>
           <LogoIcon />
         </div>
-        <div className="flex items-center space-x-5">
+        <div className="flex items-center space-x-6">
           <Link
-            className="rounded-full bg-blue-600 px-4 py-2 text-sm font-semibold text-white"
+            className="rounded-full bg-blue-600 px-4 py-2.5 text-sm font-semibold tracking-wide text-white"
             href="/"
           >
             Get started
