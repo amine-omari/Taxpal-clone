0d47013144ebf46ee520d603112f51d9a1649788
amine alaoui <omari1amineco@gmail.com>
2024-01-11 19:49:08 +0100

    fix: Xmark icon

--- icons/Xmark.jsx
+++ icons/Xmark.jsx
@@ -4,18 +4,12 @@ const Xmark = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
+      viewBox="0 0 20 20"
+      fill="currentColor"
       width={24}
       height={24}
     >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M6 18 18 6M6 6l12 12"
-      />
+      <path d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z" />
     </svg>
   );
 };
