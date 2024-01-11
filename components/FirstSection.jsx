14dae329f9be6f1d43d51fe01bc50b840989a6ed
amine alaoui <omari1amineco@gmail.com>
2024-01-11 21:28:43 +0100

    feat: add style to the title of the section

--- components/FirstSection.jsx
+++ components/FirstSection.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const FirstSection = () => {
   return (
-    <section>FirstSection</section>
-  )
-}
+    <section className="px-4 pb-16 pt-20">
+      <div className="text-center">
+        <h1 className="leading-[55px] text-5xl font-semibold">
+          Accounting
+          <br />
+          <span className="text-blue-600">made simple</span> for small
+          businesses.
+        </h1>
+      </div>
+    </section>
+  );
+};
 
-export default FirstSection
+export default FirstSection;
