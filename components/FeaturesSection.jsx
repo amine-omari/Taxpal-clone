e5107bde9d12d281381be2ccc0aa41e9e99feb96
amine alaoui <omari1amineco@gmail.com>
2024-01-12 15:34:25 +0100

    feat: import Container component to FeaturesSection component

--- components/FeaturesSection.jsx
+++ components/FeaturesSection.jsx
@@ -1,18 +1,10 @@
 import React from "react";
+import Container from "./Container";
 
 const FeaturesSection = () => {
   return (
-    <section className="bg-blue-600 pb-28 pt-20">
-      <div className="space-y-6 px-4">
-        <h2 className="text-[32px] font-semibold text-white">
-          Everything you need to run your books.
-        </h2>
-        <p className="text-lg text-blue-100">
-          Well everything you need if you aren’t that picky about minor details
-          like tax compliance.
-        </p>
-      </div>
-    </section>
+    <Container>
+    </Container>
   );
 };
 
