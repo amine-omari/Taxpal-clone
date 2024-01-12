00ee31ca63aaf8d1a302251fbd062c03dd7eeeff
amine alaoui <omari1amineco@gmail.com>
2024-01-13 00:40:57 +0100

    feat: add textColor prop in SectionHeader component

--- components/SectionHeader.jsx
+++ components/SectionHeader.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 
-const SectionHeader = ({ title, text }) => {
+const SectionHeader = ({ title, text, titleTextWhite = false, textColor }) => {
   return (
     <div className="space-y-6">
-      <h2 className="text-[32px] font-semibold leading-10 text-white">
+      <h2
+        className={`text-[32px] font-semibold leading-10 ${
+          titleTextWhite && "text-white"
+        }`}
+      >
         {title}
       </h2>
-      <p className="text-lg leading-8 text-blue-100">{text}</p>
+      <p className={`text-lg leading-8 ${textColor}`}>{text}</p>
     </div>
   );
 };
