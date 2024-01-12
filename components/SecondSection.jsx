891453b03875016180d62ee03dcc8f3db3cf3347
amine alaoui <omari1amineco@gmail.com>
2024-01-12 01:07:30 +0100

    feat: add texts to SecondSection component

--- components/SecondSection.jsx
+++ components/SecondSection.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const SecondSection = () => {
   return (
-    <section>SecondSection</section>
-  )
-}
+    <section>
+      <div>
+        <h2>
+          Everything you need to run your books.
+        </h2>
+        <p>
+          Well everything you need if you aren’t that picky about minor details
+          like tax compliance.
+        </p>
+      </div>
+    </section>
+  );
+};
 
-export default SecondSection
+export default SecondSection;
