6604fba0b409ecf5ba72e65da1fa6e1b6e8e0c58
amine alaoui <omari1amineco@gmail.com>
2024-01-13 00:01:24 +0100

    fix: style in SectionHeader component

--- components/SectionHeader.jsx
+++ components/SectionHeader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SectionHeader = ({ title, text }) => {
   return (
-    <div className="space-y-6 px-4">
+    <div className="space-y-6">
       <h2 className="text-[32px] font-semibold leading-10 text-white">
         {title}
       </h2>
