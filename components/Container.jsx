f07508d5290ceaf971bc63844c48158728925921
amine alaoui <omari1amineco@gmail.com>
2024-01-12 16:11:52 +0100

    feat: add a new prop in Container component

--- components/Container.jsx
+++ components/Container.jsx
@@ -6,11 +6,14 @@ const Container = ({
   text,
   bgBlue = false,
   bgDarkBlue = false,
+  padding,
   children,
 }) => {
   return (
     <section
-      className={`${bgBlue && "bg-blue-600"} ${bgDarkBlue && "bg-slate-900"}`}
+      className={`${bgBlue && "bg-blue-600"} ${
+        bgDarkBlue && "bg-slate-900"
+      } ${padding} relative`}
     >
       <div className="relative px-4">
         <SectionHeader title={title} text={text} />
