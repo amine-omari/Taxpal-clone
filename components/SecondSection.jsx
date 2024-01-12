96d8a9d2a225aefe08524e8426a9fd33832a4037
amine alaoui <omari1amineco@gmail.com>
2024-01-12 01:14:18 +0100

    feat: add padding style for the SeconSection component

--- components/SecondSection.jsx
+++ components/SecondSection.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const SecondSection = () => {
   return (
-    <section className="px-4">
+    <section className="bg-blue-600 px-4 pb-28 pt-20">
       <div className="space-y-6">
-        <h2 className="text-[32px]">Everything you need to run your books.</h2>
-        <p className="text-lg">
+        <h2 className="text-[32px] font-semibold text-white">
+          Everything you need to run your books.
+        </h2>
+        <p className="text-lg text-blue-100">
           Well everything you need if you aren’t that picky about minor details
           like tax compliance.
         </p>
