eecdf3991f8b30f426bb21b15adff12a523245aa
amine alaoui <omari1amineco@gmail.com>
2024-01-13 00:27:01 +0100

    feat: import TasksSection component to MainContainer component

--- components/MainContainer.jsx
+++ components/MainContainer.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "./HeroSection";
 import FeaturesSection from "./FeaturesSection";
+import TasksSection from "./TasksSection";
 
 const MainContainer = () => {
   return (
     <main>
       <HeroSection />
       <FeaturesSection />
+      <TasksSection />
     </main>
   );
 };
