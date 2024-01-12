df660e8198f35a60b2acd5329bc1ee6a707f4759
amine alaoui <omari1amineco@gmail.com>
2024-01-13 00:03:49 +0100

    feat: add the images of the FeaturesSection component

--- components/FeaturesSection.jsx
+++ components/FeaturesSection.jsx
@@ -1,29 +1,35 @@
 import React, { useState } from "react";
 import Container from "./Container";
+import Image from "next/image";
 
 const FeaturesSection = () => {
   const [text, setText] = useState();
+  const [imgSrc, setImgsrc] = useState();
 
   const buttonInfo = [
     {
       id: 1,
       button: "Payroll",
       text: "Keep track of everyone's salaries and whether or not they've been paid. Direct deposit not supported.",
+      src: "https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fpayroll.517af4e7.png&w=2048&q=75",
     },
     {
       id: 2,
       button: "Claim expenses",
       text: "All of your receipts organized into one place, as long as you don't mind typing in the data by hand.",
+      src: "https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fexpenses.3f331919.png&w=2048&q=75",
     },
     {
       id: 3,
       button: "VAT handling",
       text: "We only sell our software to companies who don't deal with VAT at all, so technically we do all the VAT stuff they need.",
+      src: "https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fvat-returns.7402820f.png&w=2048&q=75",
     },
     {
       id: 4,
       button: "Reporting",
       text: "Easily export your data into an Excel spreadsheet where you can do whatever the hell you want with it.",
+      src: "https://salient.tailwindui.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Freporting.2ad6f065.png&w=2048&q=75",
     },
   ];
 
@@ -31,6 +37,7 @@ const FeaturesSection = () => {
     const selectedButton = buttonInfo.find((button) => button.id === id);
     if (selectedButton) {
       setText(selectedButton.text);
+      setImgsrc(selectedButton.src);
     }
   };
 
@@ -42,15 +49,15 @@ const FeaturesSection = () => {
       bgBlue={true}
       padding="pb-28 pt-20"
     >
-      <div className="mt-16 overflow-hidden pt-10">
+      <div className="mt-16 pt-10">
         <div>
-          <div className="absolute right-0 top-60 h-[60%] w-full border border-white/10 bg-white/10"></div>
+          <div className="absolute right-0 top-60 h-[26%] w-full border border-white/10 bg-white/10"></div>
           <div className="absolute right-0 flex w-full space-x-4 overflow-x-scroll px-4 pb-4">
             {buttonInfo.map(({ id, button }) => (
               <button
                 key={id}
                 onClick={() => showText(id)}
-                className="whitespace-nowrap rounded-full px-4 py-1.5 text-lg font-medium text-blue-100 duration-300 hover:bg-white/10 hover:text-white focus:bg-white focus:text-blue-600"
+                className="whitespace-nowrap rounded-full px-4 py-1.5 text-lg font-medium text-blue-100 duration-300 hover:bg-white/10 hover:text-white focus:bg-white focus:text-blue-600 "
               >
                 {button}
               </button>
@@ -59,6 +66,15 @@ const FeaturesSection = () => {
           <p className="pt-16 text-lg text-white">
             {text ? text : buttonInfo[0].text}
           </p>
+        </div>
+        <div className="mt-10 w-[720px]">
+          <Image
+            src={imgSrc ? imgSrc : buttonInfo[0].src}
+            alt="imgs"
+            width={1085}
+            height={730}
+            className="w-full rounded-2xl"
+          />
         </div>
       </div>
     </Container>
