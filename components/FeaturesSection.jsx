72a332945a358328b9a3f36a0ca136d2257afcf5
amine alaoui <omari1amineco@gmail.com>
2024-01-12 16:12:14 +0100

    feat: add style in FeaturesSection component

--- components/FeaturesSection.jsx
+++ components/FeaturesSection.jsx
@@ -7,6 +7,8 @@ const FeaturesSection = () => {
       title="Everything you need to run your books."
       text="Well everything you need if you aren’t that picky about minor details
         like tax compliance."
+      bgBlue={true}
+      padding="pb-28 pt-20"
     ></Container>
   );
 };
