d6a9feb3564e93dfab5f573fc751f8c7131df352
amine alaoui <omari1amineco@gmail.com>
2024-01-12 15:56:31 +0100

    feat: add the title and the text in FeaturesSection componnent

--- components/FeaturesSection.jsx
+++ components/FeaturesSection.jsx
@@ -3,8 +3,11 @@ import Container from "./Container";
 
 const FeaturesSection = () => {
   return (
-    <Container>
-    </Container>
+    <Container
+      title="Everything you need to run your books."
+      text="Well everything you need if you aren’t that picky about minor details
+        like tax compliance."
+    ></Container>
   );
 };
 
