978d267ac3f344605ab83300ed7b7a63b824e8e7
amine alaoui <omari1amineco@gmail.com>
2024-01-12 15:30:48 +0100

    feat: add title and text to SectionHeader component

--- components/SectionHeader.jsx
+++ components/SectionHeader.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const SectionHeader = () => {
   return (
-    <div>SectionHeader</div>
-  )
-}
+    <div className="space-y-6 px-4">
+      <h2 className="text-[32px] font-semibold text-white">
+        Everything you need to run your books.
+      </h2>
+      <p className="text-lg text-blue-100">
+        Well everything you need if you aren’t that picky about minor details
+        like tax compliance.
+      </p>
+    </div>
+  );
+};
 
-export default SectionHeader
+export default SectionHeader;
