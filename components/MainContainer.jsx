1f04676c7cb4aa71f9260736c4e277da842c1627
amine alaoui <omari1amineco@gmail.com>
2024-01-12 01:03:54 +0100

    feat: import SecondSection component to MainContainer component

--- components/MainContainer.jsx
+++ components/MainContainer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import FirstSection from "./FirstSection";
+import SecondSection from "./SecondSection";
 
 const MainContainer = () => {
   return (
     <main>
       <FirstSection />
+      <SecondSection />
     </main>
   );
 };
