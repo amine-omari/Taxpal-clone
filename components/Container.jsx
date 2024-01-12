cd7d1035890f3e20b2c4653caad430e4f6c4534f
amine alaoui <omari1amineco@gmail.com>
2024-01-12 15:58:21 +0100

    feat: add children prop to Container component

--- components/Container.jsx
+++ components/Container.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import SectionHeader from "./SectionHeader";
 
-const Container = ({ title, text, bgBlue = false, bgDarkBlue = false }) => {
+const Container = ({
+  title,
+  text,
+  bgBlue = false,
+  bgDarkBlue = false,
+  children,
+}) => {
   return (
     <section
       className={`${bgBlue && "bg-blue-600"} ${bgDarkBlue && "bg-slate-900"}`}
     >
       <div className="relative px-4">
         <SectionHeader title={title} text={text} />
+        <div>{children}</div>
       </div>
     </section>
   );
