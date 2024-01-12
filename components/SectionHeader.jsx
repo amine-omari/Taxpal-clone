a315907dd127b4e2cb7a08a2a627c7233f47b664
amine alaoui <omari1amineco@gmail.com>
2024-01-12 16:13:54 +0100

    fix: SectionHeader component

--- components/SectionHeader.jsx
+++ components/SectionHeader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SectionHeader = ({ title, text }) => {
   return (
-    <div className="space-y-6 px-4">
+    <div className="space-y-6">
       <h2 className="text-[32px] font-semibold text-white">{title}</h2>
       <p className="text-lg text-blue-100">{text}</p>
     </div>
