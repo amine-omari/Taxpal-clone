694472d9d2a8f98c135bd9a58b97762f6fa371dc
amine alaoui <omari1amineco@gmail.com>
2024-01-12 12:58:00 +0100

    feat: move the nav tag in Header component to Navbar component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import LogoIcon from "@/icons/LogoIcon";
+import MenuIcon from "@/icons/MenuIcon";
+import Xmark from "@/icons/Xmark";
+import Link from "next/link";
+import React, { useState } from "react";
+import MenuContainer from "./MenuContainer";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [menuOpen, setMenuOpen] = useState();
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
-export default Navbar
+  return (
+    <nav className="flex items-center justify-between px-4">
+      <div>
+        <LogoIcon />
+      </div>
+      <div className="flex items-center space-x-6">
+        <Link
+          className="rounded-full bg-blue-600 px-4 py-2.5 text-sm font-semibold tracking-wide text-white hover:bg-blue-500"
+          href="/"
+        >
+          Get started
+        </Link>
+        <div>
+          <button
+            className="relative z-50 flex items-center"
+            onClick={toggleMenu}
+          >
+            {menuOpen ? <Xmark /> : <MenuIcon />}
+          </button>
+          {menuOpen && <MenuContainer />}
+          {menuOpen && (
+            <div
+              className="fixed right-0 top-0 h-full w-full bg-slate-300/50"
+              onClick={toggleMenu}
+            ></div>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
