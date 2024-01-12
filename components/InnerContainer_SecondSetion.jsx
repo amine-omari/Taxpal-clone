1ff6ce026673bfe9c9e73c60503e3e4fd53f2a6c
amine alaoui <omari1amineco@gmail.com>
2024-01-12 10:07:15 +0100

    feat: add new InnerContainer component of SecondSection component

---
+++ components/InnerContainer_SecondSetion.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InnerContainer_SecondSetion = () => {
+  return (
+    <div>InnerContainer_SecondSetion</div>
+  )
+}
+
+export default InnerContainer_SecondSetion
