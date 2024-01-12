6b4c082a3b2bf82bcf2fabfc16855ad6a8452fe8
amine alaoui <omari1amineco@gmail.com>
2024-01-12 01:10:22 +0100

    prettier fix

--- components/SecondSection.jsx
+++ components/SecondSection.jsx
@@ -4,9 +4,7 @@ const SecondSection = () => {
   return (
     <section className="px-4">
       <div className="space-y-6">
-        <h2 className="text-[32px]">
-          Everything you need to run your books.
-        </h2>
+        <h2 className="text-[32px]">Everything you need to run your books.</h2>
         <p className="text-lg">
           Well everything you need if you aren’t that picky about minor details
           like tax compliance.
