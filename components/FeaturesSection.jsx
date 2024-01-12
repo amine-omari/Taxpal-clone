c8a9e9cd8cbc9c65c05841e621f294742404b339
amine alaoui <omari1amineco@gmail.com>
2024-01-12 21:22:56 +0100

    fix: style FeaturesSection component

--- components/FeaturesSection.jsx
+++ components/FeaturesSection.jsx
@@ -42,20 +42,21 @@ const FeaturesSection = () => {
       bgBlue={true}
       padding="pb-28 pt-20"
     >
-      <div className="mt-16 pt-10">
+      <div className="mt-16 overflow-hidden pt-10">
         <div>
-          <div className="absolute right-0 flex w-full space-x-4 overflow-x-scroll pb-4">
+          <div className="absolute right-0 top-60 h-[60%] w-full border border-white/10 bg-white/10"></div>
+          <div className="absolute right-0 flex w-full space-x-4 overflow-x-scroll px-4 pb-4">
             {buttonInfo.map(({ id, button }) => (
               <button
                 key={id}
                 onClick={() => showText(id)}
-                className="whitespace-nowrap rounded-full px-4 py-1 text-lg font-medium text-blue-100 duration-300 hover:bg-white/10 hover:text-white focus:bg-white focus:text-blue-600"
+                className="whitespace-nowrap rounded-full px-4 py-1.5 text-lg font-medium text-blue-100 duration-300 hover:bg-white/10 hover:text-white focus:bg-white focus:text-blue-600"
               >
                 {button}
               </button>
             ))}
           </div>
-          <p className="pt-20 text-lg text-white">
+          <p className="pt-16 text-lg text-white">
             {text ? text : buttonInfo[0].text}
           </p>
         </div>
