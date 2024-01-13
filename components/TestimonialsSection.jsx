14860665f63875468f9c20a22db31bcf90b33793
amine alaoui <omari1amineco@gmail.com>
2024-01-13 16:16:14 +0100

    feat: add title and text props to TestimonialsSection component

--- components/TestimonialsSection.jsx
+++ components/TestimonialsSection.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import Container from "./Container";
 
 const TestimonialsSection = () => {
-  return <Container>TestimonialsSection</Container>;
+  return (
+    <Container
+      title="Loved by businesses worldwide."
+      text="Our software is so simple that people can’t help but fall in love with it. Simplicity is easy when you just skip tons of mission-critical features."
+    ></Container>
+  );
 };
 
 export default TestimonialsSection;
