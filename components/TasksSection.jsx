fec1c2c22df23c9658d5df3ead6c06f7153d57a5
amine alaoui <omari1amineco@gmail.com>
2024-01-13 01:01:13 +0100

    feat: add padding style to TasksSection component

--- components/TasksSection.jsx
+++ components/TasksSection.jsx
@@ -6,6 +6,8 @@ const TasksSection = () => {
     <Container
       title="Simplify everyday business tasks."
       text="Because you’d probably be a little confused if we suggested you complicate your everyday business tasks instead."
+      textColor="text-slate-700"
+      padding="pt-20 pb-14"
     ></Container>
   );
 };
