7e03d3948c098ac31e848b803433f5188719df9d
amine alaoui <omari1amineco@gmail.com>
2024-01-13 19:43:18 +0100

    fix: style

--- components/TestimonialsCard.jsx
+++ components/TestimonialsCard.jsx
@@ -8,24 +8,26 @@ const TestimonialsCard = ({ cardText, name, position, src }) => {
       <div className="absolute left-6 top-6 fill-slate-100">
         <CommaIcon />
       </div>
-      <blockquote>
-        <p className="text-lg leading-8">{cardText}</p>
-      </blockquote>
-      <figcaption className="mt-6 flex justify-between border-t border-slate-100 pt-6">
-        <div className="flex flex-col justify-between">
-          <h3 className="mt-1 font-semibold">{name}</h3>
-          <p className="text-sm tracking-wide text-slate-500">{position}</p>
-        </div>
-        <div>
-          <Image
-            src={src}
-            alt="profile img"
-            width={56}
-            height={56}
-            className="rounded-full"
-          />
-        </div>
-      </figcaption>
+      <div className="relative">
+        <blockquote>
+          <p className="text-lg leading-8">{cardText}</p>
+        </blockquote>
+        <figcaption className="mt-6 flex justify-between border-t border-slate-100 pt-6">
+          <div className="flex flex-col justify-between">
+            <h3 className="mt-1 font-semibold">{name}</h3>
+            <p className="text-sm tracking-wide text-slate-500">{position}</p>
+          </div>
+          <div>
+            <Image
+              src={src}
+              alt="profile img"
+              width={56}
+              height={56}
+              className="rounded-full"
+            />
+          </div>
+        </figcaption>
+      </div>
     </figure>
   );
 };
