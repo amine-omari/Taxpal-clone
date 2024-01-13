84ee98f6527fa4784723193847855ea7586565d1
amine alaoui <omari1amineco@gmail.com>
2024-01-13 13:05:03 +0100

    feat: add new ReportingIcon

---
+++ icons/ReportingIcon.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const ReportingIcon = () => {
+  return (
+    <svg aria-hidden="true" width={36} height={36} fill="none">
+      <defs>
+        <linearGradient
+          id=":R2menla:"
+          x1="11.5"
+          y1="18"
+          x2="36"
+          y2="15.5"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop offset=".194" stop-color="#fff"></stop>
+          <stop offset="1" stop-color="#6692F1"></stop>
+        </linearGradient>
+      </defs>
+      <path
+        d="m30 15-4 5-4-11-4 18-4-11-4 7-4-5"
+        stroke="url(#:R2menla:)"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></path>
+    </svg>
+  );
+};
+
+export default ReportingIcon;
