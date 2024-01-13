6f262e27723f78f9c6e408910e68bfef9263d28b
amine alaoui <omari1amineco@gmail.com>
2024-01-13 21:01:29 +0100

    feat: add text to PlansCard component

--- components/PlansCard.jsx
+++ components/PlansCard.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const PlansCard = () => {
   return (
-    <div>PlansCard</div>
-  )
-}
+    <div className="text-white">
+      <p>15$</p>
+      <h3>Small business</h3>
+      <p>Perfect for small / medium sized businesses</p>
+    </div>
+  );
+};
 
-export default PlansCard
+export default PlansCard;
