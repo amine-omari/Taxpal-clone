ffa4ab9fad46444442c32ab5a13e5b7657e4ee36
amine alaoui <omari1amineco@gmail.com>
2024-01-13 23:54:41 +0100

    feat: import QA component to QuestionsContainer component

--- components/QuestionsContainer.jsx
+++ components/QuestionsContainer.jsx
@@ -1,26 +1,10 @@
 import React from "react";
+import QA from "./QA";
 
 const QuestionsContainer = () => {
   return (
     <ul>
-      <li>
-        <h3>Does TaxPal handle VAT?</h3>
-        <p>
-          Well no, but if you move your company offshore you can probably ignore
-          it.
-        </p>
-      </li>
-      <li>
-        <h3>Can I pay for my subscription via purchase order?</h3>
-        <p>Absolutely, we are happy to take your money in all forms.</p>
-      </li>
-      <li>
-        <h3>How do I apply for a job at TaxPal?</h3>
-        <p>
-          We only hire our customers, so subscribe for a minimum of 6 months and
-          then let’s talk.
-        </p>
-      </li>
+      <QA />
     </ul>
   );
 };
