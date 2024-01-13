75a753404755d3b8716e3c271ec5d8bfe6e00e19
amine alaoui <omari1amineco@gmail.com>
2024-01-13 15:57:12 +0100

    feat: add link item to CtaBanner component

--- components/CtaBanner.jsx
+++ components/CtaBanner.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Container from "./Container";
+import Link from "next/link";
 
 const CtaBanner = () => {
   return (
@@ -16,7 +17,16 @@ const CtaBanner = () => {
       width={2347}
       height={1244}
       gradientStyle="absolute left-1/2 top-1/2 max-w-none -translate-x-1/2 -translate-y-1/2"
-    ></Container>
+    >
+      <div className="mt-10 flex justify-center">
+        <Link
+          href="/"
+          className="rounded-full bg-white px-4 py-2.5 text-sm font-semibold hover:bg-blue-50"
+        >
+          Get 6 months free
+        </Link>
+      </div>
+    </Container>
   );
 };
 
