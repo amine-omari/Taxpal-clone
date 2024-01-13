5be5c92f7c3003f8d7f1d68ce6a4e9052cfc1458
amine alaoui <omari1amineco@gmail.com>
2024-01-14 00:54:36 +0100

    feat: add link items to Footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,12 +1,34 @@
 import LogoIcon from "@/icons/LogoIcon";
+import Link from "next/link";
 import React from "react";
 
 const Footer = () => {
+  const links = [
+    {
+      id: 1,
+      text: "Features",
+    },
+    {
+      id: 2,
+      text: " Testimonials",
+    },
+    {
+      id: 3,
+      text: "Pricing",
+    },
+  ];
   return (
     <footer>
       <div className="py-16">
         <div className="flex justify-center">
           <LogoIcon />
+        </div>
+        <div>
+          {links.map(({ id, text }) => (
+            <Link key={id} href="/">
+              {text}
+            </Link>
+          ))}
         </div>
       </div>
     </footer>
