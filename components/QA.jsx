e502501b4fd5a596ee87156ce0dae4921faa2b4e
amine alaoui <omari1amineco@gmail.com>
2024-01-14 00:22:51 +0100

    fix: style in QA component

--- components/QA.jsx
+++ components/QA.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const QA = ({question, answer}) => {
   return (
     <li className="space-y-4">
-      <h3 className="text-lg font-medium">{question}</h3>
-      <p className="text-sm text-slate-700">{answer}</p>
+      <h3 className="text-lg font-medium tracking-wide">{question}</h3>
+      <p className="text-sm text-slate-700 leading-6 tracking-wide">{answer}</p>
     </li>
   );
 };
