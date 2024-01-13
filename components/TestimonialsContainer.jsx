8cdc63a602f1d8507a197d84e83c7ecfab820eba
amine alaoui <omari1amineco@gmail.com>
2024-01-13 19:59:28 +0100

    feat: add props to TestimonialsContainer component

--- components/TestimonialsContainer.jsx
+++ components/TestimonialsContainer.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import TestimonialsCard from "./TestimonialsCard";
 
-const TestimonialsContainer = () => {
+const TestimonialsContainer = ({
+  firstCardText,
+  firstCardName,
+  firstCardPosition,
+  firstCardSrc,
+  secondCardText,
+  secondCardName,
+  secondCardPosition,
+  secondCardSrc,
+}) => {
   return (
     <div>
-      <TestimonialsCard />
-      <TestimonialsCard />
+      <TestimonialsCard
+        cardText={firstCardText}
+        name={firstCardName}
+        position={firstCardPosition}
+        src={firstCardSrc}
+      />
+      <TestimonialsCard
+        cardText={secondCardText}
+        name={secondCardName}
+        position={secondCardPosition}
+        src={secondCardSrc}
+      />
     </div>
   );
 };
