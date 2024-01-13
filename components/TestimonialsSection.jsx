135aebe12c21be2cba1c92f842b07f2b0c8caec4
amine alaoui <omari1amineco@gmail.com>
2024-01-13 16:25:07 +0100

    feat: add padding style to TestimonialsSection component

--- components/TestimonialsSection.jsx
+++ components/TestimonialsSection.jsx
@@ -6,6 +6,8 @@ const TestimonialsSection = () => {
     <Container
       title="Loved by businesses worldwide."
       text="Our software is so simple that people can’t help but fall in love with it. Simplicity is easy when you just skip tons of mission-critical features."
+      textColor="text-slate-700"
+      padding="py-20"
     ></Container>
   );
 };
