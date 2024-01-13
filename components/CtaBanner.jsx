9536800441a15bad78f4842ecb6d3bbe311968e6
amine alaoui <omari1amineco@gmail.com>
2024-01-13 15:44:06 +0100

    feat: add Gradient background img to CtaBanner component

--- components/CtaBanner.jsx
+++ components/CtaBanner.jsx
@@ -11,6 +11,11 @@ const CtaBanner = () => {
       bgBlue={true}
       padding="py-32"
       textCenter="text-center"
+      gradientImg={true}
+      src="https://salient.tailwindui.com/_next/static/media/background-call-to-action.6a5a5672.jpg"
+      width={2347}
+      height={1244}
+      gradientStyle="absolute left-1/2 top-1/2 max-w-none -translate-x-1/2 -translate-y-1/2"
     ></Container>
   );
 };
