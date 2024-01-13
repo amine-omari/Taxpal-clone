fd3f80b46133880b66565dc6a00b2d7ff7762bda
amine alaoui <omari1amineco@gmail.com>
2024-01-13 22:10:38 +0100

    feat: add three arrays to PlansSection component

--- components/PlansSection.jsx
+++ components/PlansSection.jsx
@@ -3,6 +3,83 @@ import Container from "./Container";
 import PlansCard from "./PlansCard";
 
 const PlansSection = () => {
+  const smallBusinessServices = [
+    {
+      id: 1,
+      text: "Send 25 quotes and invoices",
+    },
+    {
+      id: 2,
+      text: "Connect up to 5 bank accounts",
+    },
+    {
+      id: 3,
+      text: "Track up to 50 expenses per month",
+    },
+    {
+      id: 4,
+      text: "Automated payroll support",
+    },
+    {
+      id: 5,
+      text: "Export up to 12 reports",
+    },
+    {
+      id: 6,
+      text: "Bulk reconcile transactions",
+    },
+    {
+      id: 7,
+      text: "Track in multiple currencies",
+    },
+  ];
+
+  const starterServices = [
+    {
+      id: 1,
+      text: "Send 10 quotes and invoices",
+    },
+    {
+      id: 2,
+      text: "Connect up to 2 bank accounts",
+    },
+    {
+      id: 3,
+      text: "Track up to 15 expenses per month",
+    },
+    {
+      id: 4,
+      text: "Manual payroll support",
+    },
+    {
+      id: 5,
+      text: "Export up to 3 reports",
+    },
+  ];
+
+  const enterpriseServices = [
+    {
+      id: 1,
+      text: "Send unlimited quotes and invoices",
+    },
+    {
+      id: 2,
+      text: "Connect up to 15 bank accounts",
+    },
+    {
+      id: 3,
+      text: "Track up to 200 expenses per month",
+    },
+    {
+      id: 4,
+      text: "Automated payroll support",
+    },
+    {
+      id: 5,
+      text: "Export up to 25 reports, including TPS",
+    },
+  ];
+
   return (
     <Container
       title="Simple pricing, for everyone."
