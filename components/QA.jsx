7949ebd32cfa5a10542e9a9d2508e075054126d5
amine alaoui <omari1amineco@gmail.com>
2024-01-13 23:59:33 +0100

    feat: add question and answer in QA component

--- components/QA.jsx
+++ components/QA.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const QA = () => {
   return (
-    <div>QA</div>
-  )
-}
+    <li className="space-y-4">
+      <h3 className="text-lg font-medium">Does TaxPal handle VAT?</h3>
+      <p className="text-slate-700 text-sm">
+        Well no, but if you move your company offshore you can probably ignore
+        it.
+      </p>
+    </li>
+  );
+};
 
-export default QA
+export default QA;
