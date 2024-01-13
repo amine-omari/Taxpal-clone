f72d8deb28fabc6b41951e2e52bec62d6003f621
amine alaoui <omari1amineco@gmail.com>
2024-01-13 23:44:04 +0100

    feat: add question and answers in QuestionsContainer component

--- components/QuestionsContainer.jsx
+++ components/QuestionsContainer.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
 const QuestionsContainer = () => {
   return (
-    <div>QuestionsContainer</div>
-  )
-}
+    <ul>
+      <li>
+        <h3>Does TaxPal handle VAT?</h3>
+        <p>
+          Well no, but if you move your company offshore you can probably ignore
+          it.
+        </p>
+      </li>
+      <li>
+        <h3>Can I pay for my subscription via purchase order?</h3>
+        <p>Absolutely, we are happy to take your money in all forms.</p>
+      </li>
+      <li>
+        <h3>How do I apply for a job at TaxPal?</h3>
+        <p>
+          We only hire our customers, so subscribe for a minimum of 6 months and
+          then let’s talk.
+        </p>
+      </li>
+    </ul>
+  );
+};
 
-export default QuestionsContainer
+export default QuestionsContainer;
