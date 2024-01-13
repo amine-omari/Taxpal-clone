8b96bfa03ef3f18d80d23b4150c74fcbdcf88954
amine alaoui <omari1amineco@gmail.com>
2024-01-13 23:05:40 +0100

    fgeat: add title and padding in FaqSection component

--- components/FaqSection.jsx
+++ components/FaqSection.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import Container from "./Container";
 
 const FaqSection = () => {
-  return <Container>FaqSection</Container>;
+  return (
+    <Container
+      title="Frequently asked questions"
+      text="If you can’t find what you’re looking for, email our support team and if you’re lucky someone will get back to you."
+      bgLightGray={true}
+      padding="py-20"
+    ></Container>
+  );
 };
 
 export default FaqSection;
