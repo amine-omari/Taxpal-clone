49f93d3bb2c61f6be049f95b9ad7870ee3f722ef
amine alaoui <omari1amineco@gmail.com>
2024-01-14 00:48:42 +0100

    feat: import LogoIcon to Footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import LogoIcon from "@/icons/LogoIcon";
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
-  )
-}
+    <footer>
+      <div>
+        <div className="flex justify-center">
+          <LogoIcon />
+        </div>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
