4ebe515a7d9296c8127208a61f878386803e93c2
amine alaoui <omari1amineco@gmail.com>
2024-01-13 15:25:27 +0100

    feat: add bg style to CtaBanner component

--- components/CtaBanner.jsx
+++ components/CtaBanner.jsx
@@ -6,6 +6,9 @@ const CtaBanner = () => {
     <Container
       title="Get started today"
       text="It’s time to take control of your books. Buy our software so you can feel like you’re doing something productive."
+      titleTextWhite={true}
+      textColor="text-white"
+      bgBlue={true}
     ></Container>
   );
 };
