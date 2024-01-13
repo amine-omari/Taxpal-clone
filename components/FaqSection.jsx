e4e03b2c1a5d5270aa6a9ac6f28ab28d7cf50904
amine alaoui <omari1amineco@gmail.com>
2024-01-13 23:44:23 +0100

    fix: text style in FaqSection component

--- components/FaqSection.jsx
+++ components/FaqSection.jsx
@@ -7,6 +7,7 @@ const FaqSection = () => {
     <Container
       title="Frequently asked questions"
       text="If you can’t find what you’re looking for, email our support team and if you’re lucky someone will get back to you."
+      textColor="text-slate-700"
       bgLightGray={true}
       padding="py-20"
     >
