bfaa4b4ea02741bd8c617ecb6a51e16263c5fc65
amine alaoui <omari1amineco@gmail.com>
2024-01-14 00:01:24 +0100

    feat: ad props to QA component

--- components/QA.jsx
+++ components/QA.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 
-const QA = () => {
+const QA = ({question, answer}) => {
   return (
     <li className="space-y-4">
-      <h3 className="text-lg font-medium">Does TaxPal handle VAT?</h3>
-      <p className="text-slate-700 text-sm">
-        Well no, but if you move your company offshore you can probably ignore
-        it.
-      </p>
+      <h3 className="text-lg font-medium">{question}</h3>
+      <p className="text-sm text-slate-700">{answer}</p>
     </li>
   );
 };
