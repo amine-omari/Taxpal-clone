e99fe84f96c1bf61707237235cd99ae5f7d9455c
amine alaoui <omari1amineco@gmail.com>
2024-01-14 16:21:02 +0100

    fix: style in Footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
   ];
   return (
     <footer className="bg-slate-50">
-      <div className="px-4">
+      <div className="px-4 sm:px-6">
         <div className="py-16">
           <div className="flex justify-center">
             <LogoIcon />
@@ -31,14 +31,14 @@ const Footer = () => {
               <Link
                 key={id}
                 href="/"
-                className="rounded-lg px-2 py-1 hover:bg-slate-100 hover:text-slate-900 duration-300"
+                className="rounded-lg px-2 py-1 duration-300 hover:bg-slate-100 hover:text-slate-900"
               >
                 {text}
               </Link>
             ))}
           </div>
         </div>
-        <div className="border-t border-slate-400/10 py-10">
+        <div className="flex flex-col border-t border-slate-400/10 py-10 sm:flex-row-reverse sm:justify-between">
           <div className="flex justify-center space-x-6">
             <Link
               href="https://twitter.com/"
@@ -53,10 +53,8 @@ const Footer = () => {
               <Github />
             </Link>
           </div>
-          <div className="mt-6 flex justify-center text-slate-500">
-            <p>
-              Copyright © 2024 TaxPal. All rights reserved.
-            </p>
+          <div className="mt-6 sm:text-sm flex justify-center text-slate-500 sm:mt-0">
+            <p>Copyright © 2024 TaxPal. All rights reserved.</p>
           </div>
         </div>
       </div>
