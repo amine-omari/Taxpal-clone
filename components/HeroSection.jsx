e63b0acd16437aef2466e990eb562aa8fc16173f
amine alaoui <omari1amineco@gmail.com>
2024-01-14 11:33:52 +0100

    feat:add responsive style for mobile in HeroSection component

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -21,17 +21,17 @@ const HeroSection = () => {
   return (
     <section className="px-4 pb-16 pt-20 sm:px-6">
       <div className="text-center">
-        <h1 className="text-5xl font-semibold leading-[55px]">
+        <h1 className="text-5xl font-semibold leading-[55px] sm:text-7xl sm:leading-[77px]">
           Accounting
           <br />
           <span className="text-blue-600">made simple</span> for small
           businesses.
         </h1>
-        <p className="mt-6 px-2 text-lg leading-8 text-slate-700">
+        <p className="mt-6 px-2 text-lg leading-8 text-slate-700 sm:mt-7">
           Most bookkeeping software is accurate, but hard to use. We make the
           opposite trade-off, and hope you don’t get audited.
         </p>
-        <div className="mt-10 flex justify-center space-x-5">
+        <div className="mt-10 flex justify-center space-x-5 sm:pt-1">
           {buttonStyle.map(({ id, text, icon, style }) => (
             <Link
               key={id}
