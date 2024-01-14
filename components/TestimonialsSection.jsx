379bd14b0828b7282fbde8b8681465893385cbc0
amine alaoui <omari1amineco@gmail.com>
2024-01-14 15:40:50 +0100

    feat: add style for TestimonialsSection component

--- components/TestimonialsSection.jsx
+++ components/TestimonialsSection.jsx
@@ -56,7 +56,7 @@ const TestimonialsSection = () => {
       title="Loved by businesses worldwide."
       text="Our software is so simple that people can’t help but fall in love with it. Simplicity is easy when you just skip tons of mission-critical features."
       textColor="text-slate-700"
-      padding="py-20"
+      padding="py-20 sm:py-32"
       bgLightGray={true}
     >
       <div className="mt-16 space-y-6">
