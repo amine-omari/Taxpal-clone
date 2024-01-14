77072b01fa30b79f26799064196d21ec9bf77512
amine alaoui <omari1amineco@gmail.com>
2024-01-14 01:14:19 +0100

    fix: Footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -21,36 +21,38 @@ const Footer = () => {
   ];
   return (
     <footer>
-      <div className="py-16">
-        <div className="flex justify-center">
-          <LogoIcon />
-        </div>
-        <div className="mt-10 flex justify-center space-x-6 text-sm text-slate-700">
-          {links.map(({ id, text }) => (
-            <Link
-              key={id}
-              href="/"
-              className="rounded-lg px-2 py-1 hover:bg-slate-100 hover:text-slate-900"
-            >
-              {text}
-            </Link>
-          ))}
-        </div>
-      </div>
       <div>
-        <div className="flex justify-center space-x-6">
-          <Link
-            href="https://twitter.com/"
-            className="fill-slate-500 duration-300 hover:fill-slate-700"
-          >
-            <Twitter />
-          </Link>
-          <Link
-            href="https://twitter.com/"
-            className="fill-slate-500 duration-300 hover:fill-slate-700"
-          >
-            <Github />
-          </Link>
+        <div className="py-16">
+          <div className="flex justify-center">
+            <LogoIcon />
+          </div>
+          <div className="mt-10 flex justify-center space-x-6 text-sm text-slate-700">
+            {links.map(({ id, text }) => (
+              <Link
+                key={id}
+                href="/"
+                className="rounded-lg px-2 py-1 hover:bg-slate-100 hover:text-slate-900"
+              >
+                {text}
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div>
+          <div className="flex justify-center space-x-6">
+            <Link
+              href="https://twitter.com/"
+              className="fill-slate-500 duration-300 hover:fill-slate-700"
+            >
+              <Twitter />
+            </Link>
+            <Link
+              href="https://twitter.com/"
+              className="fill-slate-500 duration-300 hover:fill-slate-700"
+            >
+              <Github />
+            </Link>
+          </div>
         </div>
       </div>
     </footer>
