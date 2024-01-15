ee572ea84cf0c6be3049346b4f4fc5b6972336ba
amine alaoui <omari1amineco@gmail.com>
2024-01-15 01:22:56 +0100

    feat: add function for button in FeaturesSection component

--- components/FeaturesSection.jsx
+++ components/FeaturesSection.jsx
@@ -59,7 +59,8 @@ const FeaturesSection = () => {
       gradientStyle="absolute left-1/2 top-1/2 max-w-none translate-x-[-44%] translate-y-[-42%]"
       textLarge={true}
     >
-      <div className="mt-16 pt-10 md:mt-20 lg:flex">
+      <div className="mt-16 pt-10 md:mt-20 lg:flex lg:pt-0">
+        {/* mobile / tablet screen */}
         <div className="sm:relative lg:hidden">
           <div className="absolute right-0 top-[232px] h-[27%] w-full border border-white/10 bg-white/10 sm:-top-[38px] sm:h-[228px] sm:rounded-xl"></div>
           <div className="absolute right-0 flex w-full justify-between space-x-4 overflow-x-scroll px-4 pb-4 sm:justify-center sm:px-0 md:overflow-hidden">
@@ -77,15 +78,18 @@ const FeaturesSection = () => {
             {text ? text : buttonInfo[0].text}
           </p>
         </div>
+
+        {/* from lg screen */}
         <div className="hidden lg:flex lg:items-center">
           <div className="flex flex-col text-white">
             {buttonInfo.map(({ id, button, text }) => (
               <button
+                onClick={() => showText(id)}
                 className="group min-w-[400px] rounded-l-xl border border-transparent p-6 text-left hover:bg-white/5 focus:border-white/10 focus:bg-white/10"
                 key={id}
               >
                 <h3 className="text-lg font-medium tracking-wide">{button}</h3>
-                <p className="mt-2 text-sm leading-6 text-blue-100 group-hover:text-white group-focus:text-white">
+                <p className="mt-2.5 text-sm leading-6 tracking-wide text-blue-100 group-hover:text-white group-focus:text-white">
                   {text}
                 </p>
               </button>
@@ -98,7 +102,7 @@ const FeaturesSection = () => {
             alt="imgs"
             width={1085}
             height={730}
-            className="rounded-xl shadow-xl shadow-blue-900/20"
+            className="rounded-xl shadow-xl shadow-blue-900/20 lg:rounded-2xl"
           />
         </div>
       </div>
