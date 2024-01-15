5670bbe100a56e21ed46d12956fcd65609beb712
amine alaoui <omari1amineco@gmail.com>
2024-01-15 15:54:15 +0100

    fix: _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Taxpal-clone</title>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
