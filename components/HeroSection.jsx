89f1269587564ef275f296d242bbfb75553326a0
amine alaoui <omari1amineco@gmail.com>
2024-01-16 01:21:29 +0100

    feat: add links to the HeroSection component

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -8,12 +8,14 @@ const HeroSection = () => {
     {
       id: 1,
       text: "Get 6 months free",
+      link: "https://salient.tailwindui.com/register",
       icon: false,
       style: "bg-black text-white font-semibold hover:bg-slate-700",
     },
     {
       id: 2,
       text: "Watch video",
+      link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
       icon: true,
       style: "border space-x-3 hover:border-slate-300",
     },
@@ -32,11 +34,11 @@ const HeroSection = () => {
           opposite trade-off, and hope you don’t get audited.
         </p>
         <div className="mt-10 flex justify-center space-x-5 lg:pt-0.5">
-          {buttonStyle.map(({ id, text, icon, style }) => (
+          {buttonStyle.map(({ id, link, text, icon, style }) => (
             <Link
               key={id}
               className={`${style} flex items-center rounded-full px-4 py-2 text-sm tracking-wide`}
-              href="/"
+              href={link}
             >
               <span>{icon && <Play />}</span>
               <p>{text}</p>
