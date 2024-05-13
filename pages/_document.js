aa21698fd38504dc66627e794d1d713433fd5db6
amine alaoui <omari1amineco@gmail.com>
2024-05-13 22:36:54 +0100

    fix: app icon

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>TaxPal Clone</title>
+        <link rel="icon" href="/Logo.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
